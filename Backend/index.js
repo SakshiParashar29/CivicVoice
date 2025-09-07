922033ab2d03ef703dc87b655c3f6f0b2999f9d3
Sakshi Kumari <Sakshiparashar2910@gmail.com>
2025-09-07 17:40:50 +0530

    cors updated

--- Backend/index.js
+++ Backend/index.js
@@ -7,8 +7,8 @@ import userRoutes from "./Routes/userRoutes.js";
 
 const app = express();
 
-app.use(cors({ origin: 'https://civic-voice-ten.vercel.app', credentials: true }));
-app.options('*', cors());
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
